0bff35edc9da173a9a26a350b600a568a25526c9
Dan Gibbs <daniel.gibbs@gmail.com>
2013-07-28 01:13:30 +0100

    Initial build scripts

---
+++ _build/data/prettify/transport.plugins.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * plugins transport file for Prettify extra
+ *
+ * Copyright 2013 by Dan Gibbs <dan@goldcoastmedia.co.uk>
+ * Created on 07-28-2013
+ *
+ * @package prettify
+ * @subpackage build
+ */
+
+if (! function_exists('stripPhpTags')) {
+    function stripPhpTags($filename) {
+        $o = file_get_contents($filename);
+        $o = str_replace('<' . '?' . 'php', '', $o);
+        $o = str_replace('?>', '', $o);
+        $o = trim($o);
+        return $o;
+    }
+}
+/* @var $modx modX */
+/* @var $sources array */
+/* @var xPDOObject[] $plugins */
+
+
+$plugins = array();
+
+$plugins[1] = $modx->newObject('modPlugin');
+$plugins[1]->fromArray(array (
+  'id' => 1,
+  'description' => 'Prettify snippet for parsing MODX [[ ]] tags',
+  'name' => 'Prettify',
+), '', true, true);
+$plugins[1]->setContent(file_get_contents($sources['source_core'] . '/elements/plugins/prettify.plugin.php'));
+
+return $plugins;
---
+++ _build/data/prettify/transport.propertysets.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * propertySets transport file for Prettify extra
+ *
+ * Copyright 2013 by Dan Gibbs <dan@goldcoastmedia.co.uk>
+ * Created on 07-28-2013
+ *
+ * @package prettify
+ * @subpackage build
+ */
+
+if (! function_exists('stripPhpTags')) {
+    function stripPhpTags($filename) {
+        $o = file_get_contents($filename);
+        $o = str_replace('<' . '?' . 'php', '', $o);
+        $o = str_replace('?>', '', $o);
+        $o = trim($o);
+        return $o;
+    }
+}
+/* @var $modx modX */
+/* @var $sources array */
+/* @var xPDOObject[] $propertySets */
+
+
+$propertySets = array();
+
+$propertySets[1] = $modx->newObject('modPropertySet');
+$propertySets[1]->fromArray(array (
+  'id' => 1,
+  'name' => 'Prettify',
+  'description' => 'Prettify snippet property set',
+), '', true, true);
+return $propertySets;
